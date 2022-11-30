bfd79486925a948196c3276e7048bc98451249a4
Josue Perez <josueperez723@gmail.com>
2022-11-29 21:33:43 -0400

    generator

---
+++ 09-generator.js
@@ -0,0 +1,19 @@
+// Es un tipo de iterador personalizado, que mantiene o recuerda su estado
+// Esta es una funcion de iteracion que recuerda su estado, de forma que cada vez que se ejecuta
+// empieza en la posicion del array donde termino en la anterior ejecucion
+
+function* iterate(array){
+    for (let value of array){
+        yield value
+    }
+}
+
+const it = iterate(['Josue', 'Carmen', 'Pepe', 'Pedro'])
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+
+// En cada ejecucion llama al siguiente elemento del array, cuando termina el array devuelve undefined
